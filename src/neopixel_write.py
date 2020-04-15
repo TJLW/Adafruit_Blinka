13d570551be9ad539706bc376e22f69b49a902fa
Taylor Whitaker <tjlwhitaker@gmail.com>
2020-04-15 00:34:20 -0500

    Update neopixel_write.py

--- src/neopixel_write.py
+++ src/neopixel_write.py
@@ -13,7 +13,7 @@ from adafruit_blinka.agnostic import detector
 
 if detector.board.any_raspberry_pi:
     from adafruit_blinka.microcontroller.bcm283x import neopixel as _neopixel
-elif detector.board.ULRA96:
+elif detector.board.ULTRA96:
     from adafruit_blinka.microcontroller.zynqmp import neopixel as _neopixel
 elif "sphinx" in sys.modules:
     pass
