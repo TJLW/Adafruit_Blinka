19f6853b6f3caac7de79abf663bee00c7459be3e
Taylor JL Whitaker <taylor.whitaker@astrobotic.com>
2021-03-26 19:39:03 -0400

    Temp fix for zynqmp pin.py 4

--- src/adafruit_blinka/microcontroller/zynqmp/neopixel.py
+++ src/adafruit_blinka/microcontroller/zynqmp/neopixel.py
@@ -41,8 +41,8 @@ def neopixel_write(gpio, buf):
         devmem = os.open("/dev/mem", os.O_RDWR | os.O_SYNC | os.O_CREAT)
         _neopixel_controller = mmap.mmap(devmem, NEOPIXEL_MEM_SIZE, flags = mmap.MAP_SHARED, prot = mmap.PROT_WRITE | mmap.PROT_READ, offset = NEOPIXEL_BASE_ADDRESS)
 
-        m.seek(NUM_PIXEL_OFFSET)
-        m.write_byte(NEOPIXEL_COUNT)
+        _neopixel_controller.seek(NUM_PIXEL_OFFSET)
+        _neopixel_controller.write_byte(NEOPIXEL_COUNT)
 
         # _neopixel_controller = MMIO(NEOPIXEL_BASE_ADDRESS, NEOPIXEL_MEM_SIZE)
         # _neopixel_controller.write(NEOPIXEL_COUNT_REG_OFFSET, NEOPIXEL_COUNT)
@@ -54,14 +54,14 @@ def neopixel_write(gpio, buf):
         b = buf[bpp*i+2]
         if bpp == 3:
             # pixel = (r << 16) | (g << 8) | b
-            m.seek(PIXELS_OFFSET + RED_OFFSET)
-            m.write_byte(r)
+            _neopixel_controller.seek(PIXELS_OFFSET + RED_OFFSET)
+            _neopixel_controller.write_byte(r)
 
-            m.seek(PIXELS_OFFSET + GREEN_OFFSET)
-            m.write_byte(g)
+            _neopixel_controller.seek(PIXELS_OFFSET + GREEN_OFFSET)
+            _neopixel_controller.write_byte(g)
 
-            m.seek(PIXELS_OFFSET + BLUE_OFFSET)
-            m.write_byte(b)
+            _neopixel_controller.seek(PIXELS_OFFSET + BLUE_OFFSET)
+            _neopixel_controller.write_byte(b)
 
 
         # _neopixel_controller.write(NEOPIXEL_REG_0_OFFSET + (0x4 * i), pixel)
